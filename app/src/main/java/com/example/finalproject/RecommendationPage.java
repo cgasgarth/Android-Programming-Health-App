94effc42eafac3b8a9ac06c27dd21218f63ea8a7
justin840 <justin.barrera@richmond.edu>
2022-04-12 11:15:46 -0400

    Works

--- app/src/main/java/com/example/finalproject/RecommendationPage.java
+++ app/src/main/java/com/example/finalproject/RecommendationPage.java
@@ -2,7 +2,9 @@ package com.example.finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class RecommendationPage extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class RecommendationPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recommendation_page);
+    }
+
+    public void ReturnHome(View view){
+        Intent homeIntent = new Intent(this, HomePage.class);
+        startActivity(homeIntent);
     }
 }
