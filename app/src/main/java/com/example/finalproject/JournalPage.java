7b00402606f71c8c10af966d99d9ecd745ec2869
justin840 <justin.barrera@richmond.edu>
2022-04-21 11:09:06 -0400

    Squashed commit of the following:
    commit f2cc07839ffc552f1d95bfc82a246eaedd3ef722
    Author: justin840 <justin.barrera@richmond.edu>
    Date:   Thu Apr 21 11:08:49 2022 -0400
    
        issues

--- app/src/main/java/com/example/finalproject/JournalPage.java
+++ app/src/main/java/com/example/finalproject/JournalPage.java
@@ -2,7 +2,9 @@ package com.example.finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class JournalPage extends AppCompatActivity {
 
@@ -11,4 +13,6 @@ public class JournalPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_journal_page);
     }
+
+
 }
