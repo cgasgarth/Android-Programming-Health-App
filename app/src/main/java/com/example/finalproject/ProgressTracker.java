d9100cc3a490fb470f98402cebcf496a6e89123f
cgasgarth <64235119+cgasgarth@users.noreply.github.com>
2022-04-21 18:40:39 -0400

    Progress tracker what if

--- app/src/main/java/com/example/finalproject/ProgressTracker.java
+++ app/src/main/java/com/example/finalproject/ProgressTracker.java
@@ -1,10 +1,11 @@
 package com.example.finalproject;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ProgressBar;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class ProgressTracker extends AppCompatActivity {
 
@@ -12,6 +13,8 @@ public class ProgressTracker extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_progress_tracker);
+        ProgressBar progressBar = findViewById(R.id.progressBar2);
+        progressBar.setProgress(50);
     }
 
 
